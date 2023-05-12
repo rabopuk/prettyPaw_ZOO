ea572733d59e2000378d021d644bd262fb6609a2
rabopuk <rabopuk@gmail.com>
2023-05-12 19:21:07 +0400

    add: SCSS styles for header, navigation and hero sections, also add video background to hero section

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import './index.html';
 import './index.scss'; // Добавлять библиотеки до своих стилей, чтоб можно было перекрыть
 
 //use modules
+const videoBG = document.querySelector('.video-bg');
+videoBG.innerHTML = `
+  <source src="video/video.mp4" type="video/mp4">
+  <source src="video/video.webm" type="video/webm">
+`;
