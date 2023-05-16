e870fa5ad7bed15e49e377cf667240187c76b8e0
rabopuk <rabopuk@gmail.com>
2023-05-16 21:21:48 +0400

    feat: init slider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import './index.html';
 
-// new modules
-// !Добавлять библиотеки до своих стилей, чтоб можно было перекрывать своими стилями
+
+// new modules  // !Добавлять библиотеки до своих стилей, чтоб можно было перекрывать своими стилями
+
 import 'swiper/scss';
 import 'swiper/scss/pagination';
+import { slidersInit } from './modules/sliders';
 import './index.scss';
 
+
 //use modules
+
+slidersInit('.about__slider', {
+  pagination: {
+    el: '.about__slider-pagination',
+  },
+});
+
+// https://swiperjs.com/swiper-api#param-breakpoints
+// slidersInit('.', {
+//   pagination: {
+//     el: '.about__slider-pagination',
+//   },
+// });
+
+
 const videoBG = document.querySelector('.video-bg');
 videoBG.innerHTML = `
   <source src="video/video.mp4" type="video/mp4">
