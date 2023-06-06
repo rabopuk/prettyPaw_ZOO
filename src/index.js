c94475e9738df16a7d60bb2df935371bd7d0dffa
rabopuk <rabopuk@gmail.com>
2023-06-06 18:44:01 +0400

    feat: add button for scroll to the top

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { slidersInit } from './modules/sliders';
 import { videoBackgroundInit } from './modules/videoBackground';
 import { menuControl } from './modules/menuControl';
 import { locationHover } from './modules/locationHover';
+import { initScrollButton } from './modules/scrollTopButton';
 
 import './index.scss';
 
@@ -18,68 +19,71 @@ import './index.scss';
 videoBackgroundInit();
 menuControl();
 locationHover();
+initScrollButton('arrow-top', {
+	hover: false,
+});
 
 slidersInit('.about__slider', {
-  pagination: {
-    el: '.about__slider-pagination',
-    enabled: true,
-  },
+	pagination: {
+		el: '.about__slider-pagination',
+		enabled: true,
+	},
 });
 
 const careerImageItems = document.querySelectorAll('.career__image-item');
 
 careerImageItems.forEach((item, i) => {
-  item.classList.add(`career__image-item_${i % 2 ? 'even' : 'odd'}`);
+	item.classList.add(`career__image-item_${i % 2 ? 'even' : 'odd'}`);
 });
 
 // https://swiperjs.com/swiper-api#param-breakpoints
 slidersInit('.career__slider', {
-  pagination: {
-    el: '.career__slider-pagination',
-    enabled: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 'auto',
-      spaceBetween: 12,
-      pagination: {
-        enabled: false,
-      }
-    },
-    1024: {
-      slidesPerView: 'auto',
-      spaceBetween: 16,
-      pagination: {
-        enabled: false,
-      }
-    },
-    1240: {
-      slidesPerView: 'auto',
-      spaceBetween: 16,
-      pagination: {
-        enabled: false,
-      }
-    },
-    1600: {
-      slidesPerView: 'auto',
-      spaceBetween: 24,
-      pagination: {
-        enabled: false,
-      }
-    },
-    1920: {
-      slidesPerView: 'auto',
-      spaceBetween: 32,
-      pagination: {
-        enabled: false,
-      }
-    },
-    2560: {
-      slidesPerView: 'auto',
-      spaceBetween: 40,
-      pagination: {
-        enabled: false,
-      }
-    },
-  }
+	pagination: {
+		el: '.career__slider-pagination',
+		enabled: true,
+	},
+	breakpoints: {
+		768: {
+			slidesPerView: 'auto',
+			spaceBetween: 12,
+			pagination: {
+				enabled: false,
+			}
+		},
+		1024: {
+			slidesPerView: 'auto',
+			spaceBetween: 16,
+			pagination: {
+				enabled: false,
+			}
+		},
+		1240: {
+			slidesPerView: 'auto',
+			spaceBetween: 16,
+			pagination: {
+				enabled: false,
+			}
+		},
+		1600: {
+			slidesPerView: 'auto',
+			spaceBetween: 24,
+			pagination: {
+				enabled: false,
+			}
+		},
+		1920: {
+			slidesPerView: 'auto',
+			spaceBetween: 32,
+			pagination: {
+				enabled: false,
+			}
+		},
+		2560: {
+			slidesPerView: 'auto',
+			spaceBetween: 40,
+			pagination: {
+				enabled: false,
+			}
+		},
+	}
 });
